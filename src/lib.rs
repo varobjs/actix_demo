186f967c75677b63ca9d54ab276bd3eab75f8c76
varobj.mp16 <varobj@varobj.com>
2021-09-12 20:14:02 +0800

    init

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,23 @@ extern crate diesel;
 
 pub mod models;
 pub mod schema;
+
+use diesel::prelude::*;
+
+pub fn mysql_connection() -> MysqlConnection {
+    dotenv::dotenv().ok();
+    let mysql_url = std::env::var("DATABASE_URL").unwrap();
+    MysqlConnection::establish(&mysql_url).unwrap()
+}
+
+mod tests {
+    #[test]
+    fn test_mysql_connection() {
+        use crate::mysql_connection;
+        use diesel::prelude::*;
+
+        let conn = mysql_connection();
+        let row = conn.execute("select 1").unwrap();
+        assert_eq!(row, 1);
+    }
+}
