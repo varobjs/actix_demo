1f493798d2b738833206f8f6f04ad37b9a2cf9e3
varobj <varobj@varobj.com>
2021-09-27 12:50:02 +0800

    改为文档测试

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,23 @@
 #[macro_use]
 extern crate diesel;
 
+pub mod services;
 pub mod models;
 pub mod schema;
 
 use diesel::prelude::*;
 use diesel::r2d2::{ConnectionManager, Pool};
 
-pub fn mysql_connection() -> MysqlConnection {
-    dotenv::dotenv().ok();
-    let mysql_url = std::env::var("DATABASE_URL").unwrap();
-    MysqlConnection::establish(&mysql_url).unwrap()
+/// 创建MySQL连接
+pub fn mysql_connection(url: String) -> MysqlConnection {
+    MysqlConnection::establish(&url).unwrap()
 }
 
-pub fn r2d2_mysql_connection_pool() -> Pool<ConnectionManager<MysqlConnection>> {
-    dotenv::dotenv().ok();
-    let mysql_url = std::env::var("DATABASE_URL").unwrap();
-
-    let manager: ConnectionManager<MysqlConnection> = diesel::r2d2::ConnectionManager::new(&mysql_url);
+/// 创建MySQL连接池
+pub fn r2d2_mysql_connection_pool(url: String) -> Pool<ConnectionManager<MysqlConnection>> {
+    let manager: ConnectionManager<MysqlConnection> = diesel::r2d2::ConnectionManager::new(&url);
     diesel::r2d2::Pool::builder()
         .max_size(50)
         .build(manager)
         .unwrap()
-}
-
-mod tests {
-    #[test]
-    fn test_mysql_connection() {
-        use crate::*;
-
-        let conn = mysql_connection();
-        let row = conn.execute("select 1").unwrap();
-        assert_eq!(row, 1);
-    }
-
-    #[test]
-    fn test_r2d2_mysql_connection_pool() {
-        use crate::*;
-        let pool = r2d2_mysql_connection_pool();
-        let conn = pool.get().unwrap();
-        let row = conn.execute("select 1").unwrap();
-        assert_eq!(row, 1);
-    }
 }
---
+++ src/services/users_r2d2.rs
@@ -0,0 +1,125 @@
+/// Cargo.toml
+/// [dependencies]
+/// actix-web = "3.3.2"
+/// dotenv = "0.15.0"
+/// diesel = { version = "1.4.7", features = ["mysql", "r2d2"] }
+
+// use test::Bencher;
+use diesel::r2d2::{PooledConnection, ConnectionManager};
+use diesel::{MysqlConnection, Connection, RunQueryDsl, QueryDsl, ExpressionMethods};
+use diesel::result::Error;
+
+use crate::schema::users::dsl::{id, users};
+use crate::models::users::NewUser;
+use crate::models::users::User;
+
+/// 添加用户
+///
+/// Examples:
+///
+/// ```
+/// use diesel::prelude::*;
+/// use diesel::Connection;
+/// use diesel::result::Error;
+///
+/// use hello_actix::r2d2_mysql_connection_pool;
+/// use hello_actix::services::users_r2d2;
+///
+/// dotenv::dotenv().ok();
+/// let url = std::env::var("DATABASE_URL").unwrap();
+/// let pool = r2d2_mysql_connection_pool(url);
+/// let conn = pool.get().unwrap();
+///
+/// conn.transaction::<(), _, _>(|| {
+///     let row = users_r2d2::insert_user(&conn, "deli".to_string());
+///     assert_eq!(row, 1);
+///
+///     Err(Error::RollbackTransaction)
+/// }).ok();
+/// ```
+pub fn insert_user(
+    conn: &PooledConnection<ConnectionManager<MysqlConnection>>,
+    user_name: String,
+) -> usize {
+    diesel::insert_into(users)
+        .values(&NewUser { name: user_name })
+        .execute(conn)
+        .unwrap()
+}
+
+/// 添加用户，返回user_id
+///
+/// Examples:
+///
+/// ```
+/// use diesel::prelude::*;
+/// use diesel::Connection;
+/// use diesel::result::Error;
+///
+/// use hello_actix::r2d2_mysql_connection_pool;
+/// use hello_actix::services::users_r2d2;
+///
+/// dotenv::dotenv().ok();
+/// let url = std::env::var("DATABASE_URL").unwrap();
+/// let pool = r2d2_mysql_connection_pool(url);
+/// let conn = pool.get().unwrap();
+///
+/// conn.transaction::<(), _, _>(|| {
+///     let new_id = users_r2d2::insert_user_get_id(&conn, "deli".to_string());
+///     assert!(new_id > 0);
+///
+///     Err(Error::RollbackTransaction)
+/// }).ok();
+/// ```
+pub fn insert_user_get_id(
+    conn: &PooledConnection<ConnectionManager<MysqlConnection>>,
+    user_name: String,
+) -> i32 {
+    conn.transaction::<i32, Error, _>(|| {
+        insert_user(conn, user_name);
+
+        let new_id: i32 = users
+            .select(id)
+            .order(id.desc())
+            .first(conn)
+            .unwrap();
+        Ok(new_id)
+    }).ok().unwrap()
+}
+
+/// 查询用户详情
+///
+/// Examples:
+///
+/// ```
+/// use diesel::prelude::*;
+/// use diesel::Connection;
+/// use diesel::result::Error;
+///
+/// use hello_actix::r2d2_mysql_connection_pool;
+/// use hello_actix::services::users_r2d2;
+/// use hello_actix::models::users::User;
+///
+/// dotenv::dotenv().ok();
+/// let url = std::env::var("DATABASE_URL").unwrap();
+/// let pool = r2d2_mysql_connection_pool(url);
+/// let conn = pool.get().unwrap();
+///
+/// conn.transaction::<(), _, _>(|| {
+///     let new_id = users_r2d2::insert_user_get_id(&conn, "deli".to_string());
+///     let user = users_r2d2::user_detail(&conn, new_id);
+///     println!("{:?}", user);
+///     assert_eq!(User { id: new_id, name: "deli".to_string(), is_deleted: 0 }, user);
+///
+///     Err(Error::RollbackTransaction)
+/// }).ok();
+/// ```
+pub fn user_detail(
+    conn: &PooledConnection<ConnectionManager<MysqlConnection>>,
+    user_id: i32,
+) -> User {
+    users
+        .filter(id.eq(user_id))
+        .first(conn)
+        .unwrap()
+}
